b6d3c206a340c83ba734b25d51045c8bf5b40710
jacobhefley <jacobhefley3@gmail.com>
2016-04-08 21:27:31 -0700

    ok meets requirements

---
+++ README.md
@@ -0,0 +1,31 @@
+# _Project 4_
+
+#### _Pizzology, 4/8/2016_
+
+#### By _**Jacob Hefley**_
+
+## Description
+
+_Pizza Parlor similar to Pi*ology, allows to pick size, and toppings. The backend business logic allows for much more functionality _
+
+## Setup/Installation Requirements
+
+* _web browser with CSS, javascript and HTML5 capability_
+
+## Known Bugs
+
+_none_
+
+## Support and contact details
+
+_n/a, jacobhefley3@gmail.com_
+
+## Technologies Used
+
+_HTML, CSS, Bootstrap, JavaScipt, JQuery_
+
+### License
+
+*Copyright (c) 2016 Jacob Hefley*
+
+**MIT license**
--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,26 @@ class Pizza {
 	constructor(size) {
 		  this.PizzaSize = size;
     	this.ToppingsArray = new Int8Array (14);
-      this.ToppingsArray[0]= 1; // need crust to have a pizza, so if this.Pizza Topping[0] = 1 ERROR
+      this.ToppingsArray[0]= 1; // need crust to have a pizza
       this.ToppingsArray[1]= 1;
       this.ToppingsArray[2]= 1;
       this.ToppingsToArray = {Crust: 0, Sauce: 1, Cheese: 2, Pepperoni: 3, Sausage: 4, Ham: 5, Bacon: 6, Chicken: 7, Onions: 8, Mushrooms: 9, Peppers: 10, Olives: 11, Pineapple: 12, Spinach: 13};
     	this.ToppingsToCost = {Crust: 2, Sauce: 1, Cheese: 1, Pepperoni: 1, Sausage: 1, Ham: 1, Bacon: 1, Chicken: 1, Onions: 0.5, Mushrooms: 0.5, Peppers: 0.5, Olives: 0.5, Pineapple: 0.5, Spinach: 0.5};
       this.ArrayToCost = {0: 2, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 0.5, 9: 0.5, 10: 0.5, 11: 0.5, 12: 0.5, 13: 0.5};
       this.SizeToCost = {Small: 1, Medium: 2, Large: 4}
+      // may look strange but the code maps all inputed strings to Array 
     }
   logArray(){
     return console.log(this.ToppingsArray);
   }
-  checkInput(nameIn){
-    var flag = true;
-    for (var i=0;i<14;i++){
-      if (this.ToppingsToArray[nameIn]===i){
-      }
-      else {
-        flag = false;
-      }      
-    }
-    if (flag===true){
-      this.addTopping(nameIn)
+  addToppings(tempArray){
+    for(var i=0; i<tempArray.length; i++){
+      var temp = tempArray[i];
+      var indexToAdd = this.ToppingsToArray[temp];
+      this.ToppingsArray[indexToAdd]=1;
+      console.log("added "+tempArray[i]+" to ToppingsArray["+indexToAdd+"]");
     }
   }
-  addTopping(nameIn){
-    if (this.ToppingsToArray[nameIn]===true){
-    var indexToAdd = this.ToppingsToArray[nameIn];
-    this.ToppingsArray[indexToAdd]+=1;
-    return ("added "+nameIn+" to ToppingsArray["+indexToAdd+"]");
-    }
-  }
-  	// calculateCost
   	returnCost(){
       var cost = 2;
       for(var i=1;i<8;i++){
@@ -46,49 +34,19 @@ class Pizza {
       return cost;
     }
 }
-// test of
-test = new Pizza("Large");
-
 $(function(event){
   $("#submit").click(function(event){
-    myPizza = new Pizza("Large");
-      var top = $("input:checkbox[name=q1]:checked").val();
-      myPizza.checkInput(top);
-      top = $("input:checkbox[name=q2]:checked").val();
-      myPizza.checkInput(top);
+    var sizeIn = $("#size").val();
+    myPizza = new Pizza(sizeIn);
+    var tempArray =[];
+    $("input[name='check']:checked").each(function(){
+      tempArray.push(this.value);
+    });
+    console.log(tempArray);
+      myPizza.addToppings(tempArray);
       myPizza.logArray();
-      // myPizza.logArray();
-
-      // choices[1] = $("input:radio[name=q2]:checked").val();
-      // choices[2] = $("input:radio[name=q3]:checked").val();
-      // choices[3] = $("input:radio[name=q4]:checked").val();
-      // choices[4] = $("input:radio[name=q5]:checked").val();
+     $("#output").text("Total = $"+myPizza.returnCost().toFixed(2));
     event.preventDefault();
 
   });
 });
-
-
-
-// console.log(test.addTopping("Chicken"));
-// console.log(test.addTopping("Onions"));
-// test.logArray();
-// console.log(test.returnCost());
-
-// var input = $( "form input:radio" )
-//   .wrap( "<span></span>" )
-//   .parent()
-//     .css({
-//       background: "yellow",
-//       border: "3px red solid"
-//     });
- 
-// $( "div" )
-//   .text( "For this type jQuery found " + input.length + "." )
-//   .css( "color", "red" );
- 
-// // Prevent form submission
-// $( "form" ).submit(function( event ) {
-
-//   event.preventDefault();
-// });
